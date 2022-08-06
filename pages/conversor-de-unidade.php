2ff0a6ecb48b79daf097fc3435a84a43578ab1e3
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-08-06 15:37:51 +0000

    add changes

--- pages/conversor-de-unidade.php
+++ pages/conversor-de-unidade.php
@@ -1,73 +1,89 @@
-<?= include('comp/header.php') ?>
-<?= include('comp/style.php') ?>
+<html>
 
-<h3><a href="/..">voltar</a></h3>
-<br>
-<hr><br>
-<div>
-    <form action="#" method="post">
-        <input type="number" name="value">
-        <select name="conversao" id="conversao">
-            <option value="KmToMeter">km - metro</option>
-            <option value="MeterToKm">metro - km</option>
-            <option value="MileToKm">milha - km</option>
-            <option value="KmToMile">km - milha</option>
-            <option value="CelsiusToFahrenheit">Celsius - Fahrenheit</option>
-            <option value="FahrenheitToCelsius">Fahrenheit - Celsius</option>
-        </select>
-        <button>Calcular</button>
-    </form>
-    <h3>Resultado:</h3>
-    <?php
+<head>
+    <?= include('comp/style.php') ?>
+</head>
 
-    $vl = intval($_POST["value"]);
-    $ty = $_POST["conversao"];
+<body>
+    <?= include('comp/header.php') ?>
+    <main class="content">
+        <h3>
+            <a href="/..">voltar</a>
+        </h3>
+        <div>
+            <form action="#" method="post">
+                <input type="number" name="value">
+                <select name="conversao" id="conversao">
+                    <option value="KmToMeter">km - metro</option>
+                    <option value="MeterToKm">metro - km</option>
+                    <option value="MileToKm">milha - km</option>
+                    <option value="KmToMile">km - milha</option>
+                    <option value="CelsiusToFahrenheit">Celsius - Fahrenheit</option>
+                    <option value="FahrenheitToCelsius">Fahrenheit - Celsius</option>
+                </select>
+                <button>Calcular</button>
+            </form>
+            <h3>Resultado:</h3>
 
-    echo $setTest = isset($vl) ? conversor($vl, $ty) : "Sem valores aqui";
+        </div>
+        <?php
 
-    function conversor($value, $type)
-    {
-        if (isset($value)) {
-            switch ($type) {
-                case "KmToMeter":
-                    $answer = $value * 1000;
-                    $theanswer = "$value Quilometros = $answer Metros";
-                    break;
+        $vl = intval($_POST["value"]);
+        $ty = $_POST["conversao"];
 
-                case "MeterToKm":
-                    $answer = $value / 1000;
-                    $theanswer = "$value Metros = $answer Quilometros";
-                    break;
+        echo $setTest = isset($vl) ? conversor($vl, $ty) : "Sem valores aqui";
 
-                case "KmToMile":
-                    $answer = $value * 0.621371;
-                    $theanswer = "$value Quilometros = $answer Milhas";
-                    break;
+        function conversor($value, $type)
+        {
+            if (isset($value)) {
+                switch ($type) {
+                    case "KmToMeter":
+                        $answer = $value * 1000;
+                        $theanswer = "$value Quilometros = $answer Metros";
+                        $distNeg = $value < 0 ? 'é estranho ter uma distancia negativa' : ':)';
+                        break;
 
-                case "MileToKm":
-                    $answer = $value / 0.621371;
-                    $theanswer = "$value Milhas = $answer Quilometros";
-                    break;
+                    case "MeterToKm":
+                        $answer = $value / 1000;
+                        $theanswer = "$value Metros = $answer Quilometros";
+                        $distNeg = $value < 0 ? 'é estranho ter uma distancia negativa' : ':)';
+                        break;
 
-                case "CelsiusToFahrenheit":
-                    $answer = (($value * 1.8) + 32);
-                    $theanswer = "{$value}° Celsius = {$answer}° Fahrenheit";
-                    break;
+                    case "KmToMile":
+                        $answer = $value * 0.621371;
+                        $theanswer = "$value Quilometros = $answer Milhas";
+                        $distNeg = $value < 0 ? 'é estranho ter uma distancia negativa' : ':)';
+                        break;
 
-                case "FahrenheitToCelsius":
-                    $answer = (($value - 32) / 1.8);
-                    $theanswer = "{$value}° Fahrenheit = {$answer}° Celsius";
-                    break;
+                    case "MileToKm":
+                        $answer = $value / 0.621371;
+                        $theanswer = "$value Milhas = $answer Quilometros";
+                        $distNeg = $value < 0 ? 'é estranho ter uma distancia negativa' : ':)';
+                        break;
 
-                default:
-                    $answer = "algo está errado";
+                    case "CelsiusToFahrenheit":
+                        $answer = (($value * 1.8) + 32);
+                        $theanswer = "{$value}° Celsius = {$answer}° Fahrenheit";
+                        $distNeg = $value < 0 ? 'é estranho ter uma distancia negativa' : ':)';
+                        break;
+
+                    case "FahrenheitToCelsius":
+                        $answer = (($value - 32) / 1.8);
+                        $theanswer = "{$value}° Fahrenheit = {$answer}° Celsius";
+                        $distNeg = $value < 0 ? 'é estranho ter uma distancia negativa' : ':)';
+                        break;
+
+                    default:
+                        $answer = "algo está errado";
+                }
+                return $theanswer . '<br>' . $distNeg;
+            } else {
+                return "sem valores inseridos";
             }
-            return $theanswer;
-        } else {
-            return "sem valores inseridos";
         }
-    }
-    ?>
-</div>
-
+        ?>
+    </main>
+</body>
 <?= include('comp/footer.php') ?>
+
+</html>
