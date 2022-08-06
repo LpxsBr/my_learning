35b064a9168b2d13696dddc16f3460581783df22
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-08-06 15:14:17 +0000

    ...

--- pages/comp/t.php
+++ pages/comp/t.php
@@ -1,1 +1,15 @@
+<?php
 //testing component
+
+class theyFindUs{
+  public $faa = "they find us... we need to acess the <a href='/..'>secret local</a>";
+  public function foo(){
+    echo $this->faa;
+  }
+}
+
+$instance = new theyFindUs;
+$instance -> foo();
+
+?>
+
