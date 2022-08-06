b939b90874cf0bd4aa6c95b6ebc9145db4865f3b
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-08-06 15:23:08 +0000

    more changes

--- pages/condone.php
+++ pages/condone.php
@@ -1,21 +1,32 @@
-<?=include('comp/header.php');include('comp/style.php')?>
+<html>
 
-<h3><a href="/..">voltar</a></h3>
-<br><hr><br>
-<?php
-echo "<h1>TERNARIO</h1>";
-echo "um teste com os ternarios <br>";
+<head>
+    <?= include('comp/style.php') ?>
+</head>
 
-echo "a ideia é testar se minha idade sou maior de idade ou não <br> <hr> <br>";
+<body>
+    <?= include('comp/header.php') ?>
+    <main class="content">
+        <h3>
+            <a href="/..">voltar</a>
+        </h3>
 
-$idade = 18;
+        <?php
+        echo "<h1>TERNARIO</h1>";
+        echo "um teste com os ternarios <br>";
 
-$resultado = $idade >= 18 ? "por ter $idade, é de maior" : "por ter $idade, é de menor";
+        echo "a ideia é testar se minha idade sou maior de idade ou não <br> <hr> <br>";
 
-echo $resultado;
+        $idade = 18;
+
+        $resultado = $idade >= 18 ? "por ter $idade, é de maior" : "por ter $idade, é de menor";
+
+        echo $resultado;
 
 
-?>
+        ?>
+    </main>
+</body>
+<?= include('comp/footer.php') ?>
 
-
-  
+</html>
--- pages/recursividade.php
+++ pages/recursividade.php
@@ -1,69 +1,40 @@
-<?=include('comp/header.php')?>
-<?=include('comp/style.php')?>
+<?= include('comp/header.php') ?>
+<?= include('comp/style.php') ?>
+
 <body>
-    <header class="cabecalho">
-      <div class="navbar">
-        <a href="#">Inicio</a>
-        <a href="#">Serviço</a>
-        <a href="#">Quem somos</a>
-      </div>
-    </header>
-      
     <main class="content">
-      <h3>
-        <a href="/..">voltar</a>
-      </h3>
-      <div class="menu">
-        <form method="post" action="#">
-          <input type="number" name="number" value=1>
-          <button>Calcular</Button>
-        </form>
       
-<?php
-$number = intval($_POST["number"]);
+        <h3>
+            <a href="/..">voltar</a>
+        </h3>
+      
+        <div class="menu">
+            <form method="post" action="#">
+                <input type="number" name="number" value=1>
+                <button>Calcular</Button>
+            </form>
 
+            <?php
+            $number = intval($_POST["number"]);
 
-function fatorial(int $number): float{
-  if($number <= 1){
-    $number = $number;
-  }else{
-    $number *=fatorial($number -1);
-  }
-  return $number;
-}
+            function fatorial(int $number): float
+            {
+                if ($number <= 1) {
+                    $number = $number;
+                } else {
+                    $number *= fatorial($number - 1);
+                }
+                return $number;
+            }
 
-echo "<br> fatorial($number) = ".fatorial($number);
+            echo "<br> fatorial($number) = " . fatorial($number);
 
-
-
-
-
-
-//v = x * (x-y)
-
-//v1 = x * x
-//v2 = (x - y) y-> 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
-      </div>
+            ?>
+            </d-iv>
     </main>
-  
-    <footer class="rodape">
-      h
-    </footer>
-</body>
-</html>
 
+    <?= include('comp/footer.php') ?>
+
+</body>
+
+</html>
